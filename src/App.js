6efaec134037fd93a6111138699e16842ff28c54
Diana Nazarova <alllleksandrova@mail.ru>
2024-02-21 12:57:15 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <p>start</p>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
